ae8fce0124fa48c5ee53767b048099b7490257f5
TangguhDP <tangguhmyname@gmail.com>
2019-12-28 10:53:53 +0700

    Flare added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:rive_flutter/minionController.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -20,30 +22,85 @@ class _MyAppState extends State<MyApp> {
       body: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.light,
         child: Stack(
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                height: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color(0xFF73AEF5),
-                      Color(0xFF61A4F1),
-                      Color(0xFF478DE0),
-                      Color(0xFF398AE5),
-                    ],
-                  ),
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xFF73AEF5),
+                    Color(0xFF61A4F1),
+                    Color(0xFF478DE0),
+                    Color(0xFF398AE5),
+                  ],
+                  stops: [0.1, 0.4, 0.7, 0.9],
                 ),
               ),
-              Container(
-                child: SafeArea(
-                  child: Text("Test"),
-                ),
-              )
-            ],
+            ),
+            SafeArea(
+              child: Minion(),
+            )
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class Minion extends StatefulWidget {
+  @override
+  _MinionState createState() => _MinionState();
+}
+
+class _MinionState extends State<Minion> {
+  MinionController minionController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    minionController = MinionController();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            height: 300,
+            child: FlareActor(
+              "./assets/flare/minion.flr",
+              controller: minionController,
+            ),
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                InkWell(
+                  onTap: () {
+                    minionController.dance();
+                  },
+                  child: Text("Dance"),
+                ),
+                InkWell(
+                  onTap: () {
+                    minionController.jump();
+                  },
+                  child: Text("Jump"),
+                ),
+                InkWell(
+                  onTap: () {
+                    minionController.wave();
+                  },
+                  child: Text("Wave"),
+                )
+              ],
+            )
+        ],
       ),
     );
   }
